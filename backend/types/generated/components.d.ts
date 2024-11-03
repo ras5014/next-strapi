2ca7d0f059c14197c535626045f20caadf7a24da
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2024-11-04 00:23:04 +0530

    Added fields to strapi

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,3 +1,36 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ComponentLink extends Struct.ComponentSchema {
+  collectionName: 'components_component_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    text: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    description: '';
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images' | 'files'>;
+    link: Schema.Attribute.Component<'component.link', false>;
+    subHeading: Schema.Attribute.Text;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'component.link': ComponentLink;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
+}
